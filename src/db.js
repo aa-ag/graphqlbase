eff6db48e86a2a06cd294a516a69dab2fdbb6fd0
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-29 21:06:39 -0500

    Add a third fake record

--- src/db.js
+++ src/db.js
@@ -1,4 +1,5 @@
 export let users = [
     { id: 1, name: "John Doe", email: "john@example.com", age: 21 },
-    { id: 2, name: "Jane Doe", email: "jane@example.com", age: 22 }
+    { id: 2, name: "Jane Doe", email: "jane@example.com", age: 22 },
+    { id: 3, name: "Don Doe", email: "dane@example.com", age: 50 }
   ];
